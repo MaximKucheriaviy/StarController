b40e027468ec00a0f723d9e49fc740dbb093e354
Maxim Kucheriaviy <maxim-k@i.ua>
2021-12-27 13:00:59 +0200

    ModeSystem

--- StarController.ino
+++ StarController.ino
@@ -1,6 +1,9 @@
 #include <IRremote.h>
 int RECV_PIN = 2;
 
+unsigned int mode = 0;
+unsigned int preMode = 0;
+bool update = true;
 
 IRrecv irrecv(RECV_PIN);
 decode_results results;
@@ -13,7 +16,32 @@ void setup()
 
 void loop() {
   if (irrecv.decode(&results)) {
-      Serial.println(results.value, HEX);
+      
+      if(results.value == 0x926D12ED){
+        mode = 0;
+      }
+      else if(results.value == 0x926D629D){
+        mode = 1;
+      }
+      else if(results.value == 0x926D22DD){
+        mode = 2;
+      }
+      else if(results.value == 0x926D40BF){
+        mode = 3;
+      }
+      else if(results.value == 0x926DA25D){
+        mode = 4;
+      }
+      if(mode != preMode){
+        update = true;
+        preMode = mode;
+      }
       irrecv.resume(); // Receive the next value
+  }
+
+
+  if(update){
+    Serial.println(mode);
+    update = false;
   }
 }
